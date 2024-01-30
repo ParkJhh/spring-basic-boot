a97071612770c9163047523182e1fff956dbc252
ParkJhh <qkrwnsgur145@gmail.com>
2024-01-30 13:15:48 +0900

    no message

--- src/main/java/com/kitri/springbasicboot/lesson/mapping/MappingController.java
+++ src/main/java/com/kitri/springbasicboot/lesson/mapping/MappingController.java
@@ -1,4 +1,21 @@
 package com.kitri.springbasicboot.lesson.mapping;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class MappingController {
+    //ResponseBody를 붙이면, return 값을 ResponseBody에 넣겠다
+
+    @GetMapping("/get")
+    @ResponseBody
+    public String get(){
+        return "hello";
+    }
+
+    @GetMapping("/get2")
+    public String get2(){
+
+    }
 }
---
+++ src/main/webapp/WEB-INF/views/lesson/mapping.jsp
@@ -0,0 +1,11 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+
+<html>
+<head>
+    <title>HELLO</title>
+</head>
+<body>
+<img src="/hello.gif"></img>
+    <h1>${message}</h1>
+</body>
+</html>
